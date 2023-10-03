3fc762a2cf11a516a7ad68fdfd0863ddd21872e7
sayan goswami <sayan.goswami@acquia.com>
2023-10-03 13:15:37 +0530

    Add Service Decorator

--- src/CompositeEntityReferencedByNewHostRevisionViaUntranslatableReferenceNeedingSave.php
+++ src/CompositeEntityReferencedByNewHostRevisionViaUntranslatableReferenceNeedingSave.php
@@ -2,31 +2,52 @@
 
 namespace Drupal\custom_paragraph_revision_policy;
 
+use Drupal\Core\DependencyInjection\DependencySerializationTrait;
 use Drupal\Core\Entity\TranslatableRevisionableInterface;
 use Drupal\entity_reference_revisions\Plugin\Field\FieldType\EntityReferenceRevisionsItem;
 use Drupal\entity_reference_revisions\RevisionCreationPolicy\RevisionCreationPolicyInterface;
 use Drupal\entity_reference_revisions\RevisionCreationPolicy\CompositeEntityReferencedByNewHostRevisionViaUntranslatableReference;
+
 class CompositeEntityReferencedByNewHostRevisionViaUntranslatableReferenceNeedingSave implements RevisionCreationPolicyInterface {
 
+  use DependencySerializationTrait;
+
+  /**
+   * @var \Drupal\entity_reference_revisions\RevisionCreationPolicy\RevisionCreationPolicyInterface
+   */
+  private RevisionCreationPolicyInterface $inner;
+
+  public function __construct(RevisionCreationPolicyInterface $inner) {
+    $this->inner = $inner;
+  }
+
   public function initializer(RevisionCreationPolicyInterface $chain) {
-    $chain->swapPolicies(new CompositeEntityReferencedByNewHostRevisionViaUntranslatableReference(), $this);
+    $chain->addPolicy($this);
     return $chain;
   }
 
   public function shouldCreateNewRevision(EntityReferenceRevisionsItem $item) {
-    $host = $item->getEntity();
+    //    $host = $item->getEntity();
+    //    if (
+    //      // A composite entity
+    //      $item->entity && $item->entity->getEntityType()
+    //        ->get('entity_revision_parent_id_field') &&
+    //
+    //      // Referenced by a new host revision
+    //      !$host->isNew() && $host->isNewRevision() &&
+    //
+    //      // Via an untranslatable reference
+    //      !$item->getFieldDefinition()->isTranslatable() &&
+    //
+    //      // If we need to save the entity
+    //      $item->entity->needsSave()
+    //    ) {
+    //      return TRUE;
+    //    }
     if (
-      // A composite entity
-      $item->entity && $item->entity->getEntityType()
-        ->get('entity_revision_parent_id_field') &&
-
-      // Referenced by a new host revision
-      !$host->isNew() && $host->isNewRevision() &&
-
-      // Via an untranslatable reference
-      !$item->getFieldDefinition()->isTranslatable() &&
-
-      // If we need to save the entity
+      // Previous Policy is TRUE
+      $this->inner->shouldCreateNewRevision($item) &&
+      // And Item is marked for save
       $item->entity->needsSave()
     ) {
       return TRUE;
